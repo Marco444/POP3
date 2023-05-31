ff01aab752f65cbdc4c566ae044b33af3d477f68
Marco4444 <mscilipoti@itba.edu.ar>
2023-05-31 14:35:26 -0300

    added necessary code missing in parser to initialize it correctly in the handleNewPOP3Connection

--- pop3/handleNewPOP3Connection.c
+++ pop3/handleNewPOP3Connection.c
@@ -1,22 +1,11 @@
 #include "../lib/selector/selector.h"
 #include "./pop3_states.h"
 #include "pop3.h"
+#include "parser.h"
 #include "shared.h"
 #include <sys/socket.h>
 #include <unistd.h>
 #include <stdlib.h>
-
-// int handleNewPOP3Connection() {
-//
-//      // Initialize the state machine
-//     struct state_machine stm = {
-//         .initial   = AUTHORIZATION_STATE,
-//         .states    = server_states,
-//         .max_state = SERVER_STATE_COUNT,
-//     };
-//     stm_init(&stm);
-// }
-
 
 void pop3_read(struct selector_key * key);
 void pop3_write(struct selector_key * key);
@@ -60,10 +49,10 @@ void handleNewPOP3Connection(struct selector_key * key) {
         return;
     }
 
-    // if (newClientSocket > 1023) {
-    //     close(newClientSocket);
-    //     return;
-    // }
+    if (newClientSocket > 1023) {
+        close(newClientSocket);
+        return;
+    }
 
     struct connection_state * clientData = calloc(1, sizeof(struct connection_state));
     if (clientData == NULL) {
@@ -73,8 +62,7 @@ void handleNewPOP3Connection(struct selector_key * key) {
 
     buffer_init(&clientData->commands.read_buffer, BUFFER_SIZE, clientData->commands.in_buffer);
     buffer_init(&clientData->commands.write_buffer, BUFFER_SIZE, clientData->commands.out_buffer);
-    // clientData->parser = parser_init();
-
+    clientData->parser = parser_init(parser_no_classes(), &pop3_parser_definition);
     clientData->stm.initial = AUTHORIZATION_STATE;
     clientData->stm.states = server_states;
     clientData->stm.max_state = SERVER_STATE_COUNT;
---
+++ pop3/parser.h
@@ -0,0 +1,13 @@
+
+
+#define N(x) (sizeof(x)/sizeof((x)[0]))
+
+enum pop3_parser_states {
+    POP3_STATE_CMD,
+    POP3_STATE_ARG,
+    POP3_STATE_CR,
+    POP3_STATE_LF,
+};
+
+extern const struct parser_definition pop3_parser_definition;
+
