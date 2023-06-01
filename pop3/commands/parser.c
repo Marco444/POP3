b351cbd8736997fbf6dcce2880d417557c8d4d84
Marco4444 <mscilipoti@itba.edu.ar>
2023-06-01 09:52:31 -0300

    redesigned the parser, still left handling multiple spaces

--- pop3/commands/parser.c
+++ pop3/commands/parser.c
@@ -9,51 +9,19 @@
 
 
 enum pop3_parser_states {
+    POP3_STATE_START,
     POP3_STATE_CMD,
-    POP3_STATE_ARG,
-    POP3_STATE_CR,
-    POP3_STATE_LF,
+    POP3_STATE_ARG1,
+    POP3_STATE_ARG2,
+    POP3_STATE_END,
 };
 
-static void act1_cr(struct parser_event *ret, const uint8_t c, struct commands_state * ctx);
-static void act1_cmd(struct parser_event *ret, const uint8_t c, struct commands_state * ctx);
-static void act1_arg2(struct parser_event *ret, const uint8_t c, struct commands_state * ctx);
-static void act1_arg(struct parser_event *ret, const uint8_t c, struct commands_state * ctx);
+// static void act1_cr(struct parser_event *ret, const uint8_t c, struct commands_state * ctx);
+// static void act1_cmd(struct parser_event *ret, const uint8_t c, struct commands_state * ctx);
+// static void act1_arg2(struct parser_event *ret, const uint8_t c, struct commands_state * ctx);
+static void consume_space(struct parser_event *ret, const uint8_t c, struct commands_state * ctx){}
 
-static const struct parser_state_transition ST_CMD [] =  {
-    {.when = ' ',  .dest = POP3_STATE_ARG, .act1 = act1_cmd},
-    {.when = '\r', .dest = POP3_STATE_CR,  .act1 = act1_cr},
-    {.when = ANY,  .dest = POP3_STATE_CMD, .act1 = act1_cmd},
-};
-
-static const struct parser_state_transition ST_ARG [] =  {
-    {.when = '\r', .dest = POP3_STATE_CR,  .act1 = act1_arg},
-    {.when = ' ',  .dest = POP3_STATE_ARG, .act1 = act1_arg2},
-};
-
-static const struct parser_state_transition ST_CR [] =  {
-    {.when = '\n', .dest = POP3_STATE_CMD, .act1 = act1_cr},
-};
-
-// state transition mapping
-static const struct parser_state_transition *pop3_states_transitions[] = {
-    ST_CMD, ST_ARG, ST_CR, 
-};
-
-// size of each state transition
-static const size_t pop3_states_transitions_n [] = {
-    N(ST_CMD), N(ST_ARG), N(ST_CR), 
-};
-
-// final parser definition
-const struct parser_definition pop3_parser_definition = {
-    .states_count = N(pop3_states_transitions),
-    .states       = pop3_states_transitions,
-    .states_n     = pop3_states_transitions_n,
-    .start_state  = POP3_STATE_CMD,
-};
-
-static void act1_cmd(struct parser_event *ret, const uint8_t c, struct commands_state * ctx) {
+static void consume_into_cmd(struct parser_event *ret, const uint8_t c, struct commands_state * ctx) {
     // append the character to the command
     if (ctx->cmd_length < POP3_MAX_CMD_LENGTH) {
         ctx->cmd[ctx->cmd_length++] = c;
@@ -61,7 +29,7 @@ static void act1_cmd(struct parser_event *ret, const uint8_t c, struct commands_
     }
 }
 
-static void act1_arg(struct parser_event *ret, const uint8_t c, struct commands_state * ctx) {
+static void consume_into_arg1(struct parser_event *ret, const uint8_t c, struct commands_state * ctx) {
     // append the character to the first argument
     if (ctx->arg1_length < POP3_MAX_ARG_LENGTH) {
         ctx->arg1[ctx->arg1_length++] = c;
@@ -69,15 +37,17 @@ static void act1_arg(struct parser_event *ret, const uint8_t c, struct commands_
     }
 }
 
-static void act1_arg2(struct parser_event *ret, const uint8_t c, struct commands_state * ctx) {
-    // append the character to the second argument
+
+static void consume_into_arg2(struct parser_event *ret, const uint8_t c, struct commands_state * ctx) {
+    // append the character to the first argument
     if (ctx->arg2_length < POP3_MAX_ARG_LENGTH) {
         ctx->arg2[ctx->arg2_length++] = c;
         ctx->arg2[ctx->arg2_length] = '\0';
     }
 }
 
-static void act1_cr(struct parser_event *ret, const uint8_t c, struct commands_state * ctx) {
+
+static void process_command_handler(struct parser_event *ret, const uint8_t c, struct commands_state * ctx) {
     // process the command here, then reset the command and argument buffers
     process_command(ctx);
 
@@ -85,4 +55,62 @@ static void act1_cr(struct parser_event *ret, const uint8_t c, struct commands_s
     ctx->cmd[0] = ctx->arg1[0] = ctx->arg2[0] = '\0';
     ctx->cmd_length = ctx->arg1_length = ctx->arg2_length = 0;
 }
+
+
+static void invalid_arguments(struct parser_event *ret, const uint8_t c, struct commands_state * ctx) {
+    //notify invalid command
+
+    // reset buffers
+    ctx->cmd[0] = ctx->arg1[0] = ctx->arg2[0] = '\0';
+    ctx->cmd_length = ctx->arg1_length = ctx->arg2_length = 0;
+}
+
+static const struct parser_state_transition st_start [] =  {
+    {.when = ' ',  .dest = POP3_STATE_START, .act1 = consume_space},
+    {.when = ANY,  .dest = POP3_STATE_CMD, .act1 = consume_into_cmd},
+};
+
+static const struct parser_state_transition st_cmd [] =  {
+    {.when = ' ',  .dest = POP3_STATE_ARG1, .act1 = consume_space},
+    {.when = '\r', .dest = POP3_STATE_END, .act1 = consume_space},
+    {.when = ANY,  .dest = POP3_STATE_CMD, .act1 = consume_into_cmd},
+};
+
+static const struct parser_state_transition st_arg1 [] =  {
+    {.when = ' ',  .dest = POP3_STATE_ARG2, .act1 = consume_space},
+    {.when = '\r', .dest = POP3_STATE_END, .act1 = consume_space},
+    {.when = ANY,  .dest = POP3_STATE_ARG1, .act1 = consume_into_arg1},
+};
+
+static const struct parser_state_transition st_arg2 [] =  {
+    {.when = ' ',  .dest = POP3_STATE_START, .act1 = invalid_arguments},
+    {.when = '\r', .dest = POP3_STATE_END, .act1 = consume_space},
+    {.when = ANY,  .dest = POP3_STATE_ARG2, .act1 = consume_into_arg2},
+};
+
+//should create cr for cmd, arg1, arg2 in case we have '\r' as part of the
+//desired command or argument!
+static const struct parser_state_transition st_end [] =  {
+    {.when = '\n', .dest = POP3_STATE_START, .act1 = process_command_handler},
+    {.when = ANY,  .dest = POP3_STATE_ARG2, .act1 = consume_into_arg2},
+};
+
+ // state transition mapping
+ static const struct parser_state_transition *pop3_states_transitions[] = {
+    st_start, st_cmd, st_arg1, st_arg2, st_end
+ };
+
+ // size of each state transition
+ static const size_t pop3_states_transitions_n [] = {
+     N(st_start), N(st_cmd), N(st_arg1), N(st_arg2), N(st_end)
+ };
+
+ // final parser definition
+ const struct parser_definition pop3_parser_definition = {
+     .states_count = N(pop3_states_transitions),
+     .states       = pop3_states_transitions,
+     .states_n     = pop3_states_transitions_n,
+     .start_state  = POP3_STATE_START,
+ };
+
 
