792ea852d587abb176c845a2966463dc526a126c
Lautaro hernando <lautihernando@gmail.com>
2023-06-08 09:50:39 -0300

    first implementation of RETR

--- pop3/commands/transaction/retr.c
+++ pop3/commands/transaction/retr.c
@@ -10,28 +10,29 @@ typedef struct email_data {
     int email_fd;
     char email_buffer[BUFFER_SIZE];
     buffer * connection_buffer;
-    buffer * write_buffer;
+    buffer  write_buffer;
 }email_data;
 void pop3_read_email_handler(struct selector_key *key){
     email_data * data = (email_data *) key->data;
     buffer * connection_buffer = data->connection_buffer;
-    buffer * write_buffer = data->write_buffer;
+    buffer * write_buffer = &data->write_buffer;
     size_t capacity;
     ssize_t sent;
-    if (!buffer_can_read(write_buffer)) {
-        return;
-    }
-    uint8_t * readPtr = buffer_read_ptr(write_buffer, &(capacity));
+    uint8_t * readPtr = buffer_write_ptr(write_buffer, &(capacity));
     sent = read(data->email_fd, readPtr, capacity);
     if (sent <= 0) {
         // close conection
     } else {
-        buffer_read_adv(write_buffer, sent);
+        buffer_write_adv(write_buffer, sent);
     }
-    if(buffer_can_read(write_buffer))
-        selector_set_interest_key(key, OP_WRITE);
-    else
-        selector_set_interest_key(key, OP_READ);
+    if (!buffer_can_read(write_buffer))
+    {
+        printf("Cannot read buffer\n");
+    }
+    if(!buffer_can_write(connection_buffer)){
+        printf("Cannot write buffer\n");
+    }
+    
     while (buffer_can_read(write_buffer) && buffer_can_write(connection_buffer))
     {
         uint8_t * readPtr = buffer_read_ptr(write_buffer, &(capacity));
@@ -51,7 +52,7 @@ static fd_handler handler = {
     .handle_close = NULL,
     .handle_block = NULL,
 };
-enum pop3_states handle_retr(struct commands_state * ctx, struct selector_key *key) {
+enum pop3_states handle_retr(struct commands_state * ctx, struct selector_key * key) {
     printf("RETR %s\n",ctx->email_files[atoi(ctx->arg1) - 1].path);
     int fd = open(ctx->email_files[atoi(ctx->arg1) - 1].path, O_RDONLY);
     if(fd == -1){
@@ -60,10 +61,12 @@ enum pop3_states handle_retr(struct commands_state * ctx, struct selector_key *k
     }
     email_data * data = calloc(1,sizeof(email_data));
     data->connection_buffer = &ctx->write_buffer;
-    buffer_init(data->write_buffer,BUFFER_SIZE, data->email_buffer);
+    buffer_init(&data->write_buffer,BUFFER_SIZE, data->email_buffer);
     data->email_fd = fd;
     data->connection_fd = key->fd;
+
     selector_register(key->s,fd, &handler, OP_READ, data);
+    
     elem_type elem = calloc(1,sizeof(struct cmd));
     elem->cmd_id = RETR;
     ctx->email_fd = fd;
