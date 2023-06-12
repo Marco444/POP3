d5b8c0984e992156914995c44f467d4bbb30e2bf
Marco4444 <mscilipoti@itba.edu.ar>
2023-06-12 18:37:54 -0300

    added basic logic of RSET

--- pop3/commands/transaction/rset.c
+++ pop3/commands/transaction/rset.c
@@ -1,8 +1,10 @@
 #include "../command_service.h"
+#include "../../states/write_buffer_helpers.h"
 #include <stdio.h>
 
+#define RSET_MSG "+OK\r\n"
+
 enum pop3_states handle_rset(struct commands_state * ctx,struct selector_key *key) {
-    printf("RSET\n");
     ctx->pop3_current_command->cmd_id = RSET;
     ctx->pop3_current_command->is_finished = false;
     ctx->pop3_current_command->has_error = false;
@@ -10,5 +12,12 @@ enum pop3_states handle_rset(struct commands_state * ctx,struct selector_key *ke
     return TRANSACTION_STATE;
 }
 enum pop3_states handle_write_rset(struct selector_key *key, pop3_current_command *current_command, struct commands_state *commands) {
-  return TRANSACTION_STATE;
+
+    for(int i = 0; i < commands->inbox_data.email_files_length; i++) {
+       commands->inbox_data.email_files[i].is_deleted = false; 
+    }
+
+    write_in_buffer(key, RSET_MSG, strlen(RSET_MSG), 0);
+    current_command->is_finished = true;
+    return TRANSACTION_STATE;
 }
