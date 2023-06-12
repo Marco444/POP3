e45f65d86b1708f6e9e1d0223d879e67cc0dc846
Marco4444 <mscilipoti@itba.edu.ar>
2023-06-12 19:35:23 -0300

    UPDATE state correctly deletes emails

--- pop3/states/update.c
+++ pop3/states/update.c
@@ -8,10 +8,6 @@
 
 void on_arrival_update(const unsigned state, struct selector_key *key) {
     struct commands_state *commands = (struct commands_state *)key->data;
-    bool has_place;
-    long offset;
-
-    printf("arrived update\n");
 
     for(int i = 0; i < commands->inbox_data.email_files_length; i++) {
         if(commands->inbox_data.email_files[i].is_deleted) {
@@ -23,9 +19,9 @@ void on_arrival_update(const unsigned state, struct selector_key *key) {
         }
     }
 
-    has_place = enters_the_buffer(key, SIGNOFF_MSG);
+    bool has_place = enters_the_buffer(key, SIGNOFF_MSG);
     if (has_place) {
-        offset = write_in_buffer(key, SIGNOFF_MSG, strlen(SIGNOFF_MSG), 0);
+        int offset = write_in_buffer(key, SIGNOFF_MSG, strlen(SIGNOFF_MSG), 0);
         if (offset == -1) {
             commands->pop3_current_command->is_finished = true;
         }
