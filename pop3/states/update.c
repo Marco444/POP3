44d786b2212b7528b8466597e06106a08c6449d6
Marco4444 <mscilipoti@itba.edu.ar>
2023-06-12 19:25:10 -0300

    added schema of update state arrival

--- pop3/states/update.c
+++ pop3/states/update.c
@@ -1,10 +1,35 @@
 #include "../pop3_states.h"
-#include <stdio.h>
+#include "write_buffer_helpers.h"
 
-// State function declarations for UPDATE_STATE
-void on_arrival_update(const unsigned state, struct selector_key *key){
-   puts("arrived update");
-   return;
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIGNOFF_MSG "+OK POP3 server deleted the mails\r\n"
+
+void on_arrival_update(const unsigned state, struct selector_key *key) {
+    struct commands_state *commands = (struct commands_state *)key->data;
+    bool has_place;
+    long offset;
+
+    printf("arrived update\n");
+
+    for(int i = 0; i < commands->inbox_data.email_files_length; i++) {
+        if(commands->inbox_data.email_files[i].is_deleted) {
+            int result = remove(commands->inbox_data.email_files[i].path);
+            if(result == 0)
+                printf("Email file %s deleted successfully\n", commands->inbox_data.email_files[i].name);
+            else
+                fprintf(stderr, "Failed to delete email file %s\n", commands->inbox_data.email_files[i].name);
+        }
+    }
+
+    has_place = enters_the_buffer(key, SIGNOFF_MSG);
+    if (has_place) {
+        offset = write_in_buffer(key, SIGNOFF_MSG, strlen(SIGNOFF_MSG), 0);
+        if (offset == -1) {
+            commands->pop3_current_command->is_finished = true;
+        }
+    }
 }
 
 void on_departure_update(const unsigned state, struct selector_key *key){ return; }
