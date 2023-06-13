ea9bf92707e4658a23a2896296787648ebcee235
martinippo <mippolito@itba.edu.ar>
2023-06-13 13:45:55 -0300

    Testing stat

--- pop3/commands/transaction/stat.c
+++ pop3/commands/transaction/stat.c
@@ -1,5 +1,11 @@
 #include "../command_service.h"
 #include <stdio.h>
+
+#define MAX_OUTPUT 100
+#define OK_STAT_START "+OK"
+#define OK_STAT_END "\r\n"
+#define ERRORS_STAT "-ERR\r\n"
+
 enum pop3_states handle_stat(struct commands_state * ctx, struct selector_key *key) {
      printf("STAT\n");
     ctx->pop3_current_command->cmd_id = STAT;
@@ -10,5 +16,35 @@ enum pop3_states handle_stat(struct commands_state * ctx, struct selector_key *k
 }
 
 enum pop3_states handle_write_stat(struct selector_key *key, pop3_current_command *current_command, struct commands_state *commands) {
+    if(current_command->has_error){
+        bool has_place = enters_the_buffer(key, ERRORS_STAT);
+        if(has_place){
+            long offset = write_in_buffer(key, ERRORS_STAT);
+            if(offset == -1){
+                current_command->is_finished = true;
+            }
+        }
+    }else{
+        // Contar mails e imprimirlo
+        int emails_count = 0;
+        int emails_total_size = 0;
+
+        int i;
+        for(i = 0; i < commands->inbox_data.total_size; i++){
+            if(!commands->inbox_data.email_files->is_deleted){
+                emails_total_size = commands->inbox_data.email_files->size;
+                emails_count++;
+            }
+        }
+        char output[MAX_OUTPUT];
+        sprintf(output, "%s %d %d %s", OK_STAT_START, emails_count, emails_total_size, OK_STAT_END);
+        bool has_place = enters_the_buffer(key, output);
+        if(has_place){
+            long offset = write_in_buffer(key, output);
+            if(offset == -1){
+                current_command->is_finished = true;
+            }
+        }
+    }
     return TRANSACTION_STATE;
 }
