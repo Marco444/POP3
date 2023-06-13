2469001dc51bce29781a2ac7d0b3f2b1dda30b41
martinippo <mippolito@itba.edu.ar>
2023-06-13 14:18:23 -0300

    STAT commnado is working

--- pop3/commands/transaction/stat.c
+++ pop3/commands/transaction/stat.c
@@ -30,17 +30,19 @@ enum pop3_states handle_write_stat(struct selector_key *key, pop3_current_comman
         int emails_total_size = 0;
 
         int i;
-        for(i = 0; i < commands->inbox_data.total_size; i++){
-            if(!commands->inbox_data.email_files->is_deleted){
-                emails_total_size = commands->inbox_data.email_files->size;
+        for(i = 0; i < commands->inbox_data.email_files_length; i++){
+            
+            if(!commands->inbox_data.email_files[i].is_deleted){
+                emails_total_size += commands->inbox_data.email_files[i].size;
                 emails_count++;
             }
+            
         }
         char output[MAX_OUTPUT];
         sprintf(output, "%s %d %d %s", OK_STAT_START, emails_count, emails_total_size, OK_STAT_END);
         bool has_place = enters_the_buffer(key, output);
         if(has_place){
-            long offset = write_in_buffer(key, output);
+            long offset = write_in_buffer(key, output, strlen(output), 0);
             if(offset == -1){
                 current_command->is_finished = true;
             }
