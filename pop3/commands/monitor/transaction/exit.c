d90b23302e3110f68eca39bf216ed5740860c867
Marco4444 <mscilipoti@itba.edu.ar>
2023-06-16 14:04:27 -0300

    corrected bug in reseting buffers in parser

--- pop3/commands/monitor/transaction/exit.c
+++ pop3/commands/monitor/transaction/exit.c
@@ -5,6 +5,15 @@
 #include <stdio.h>
 
 enum monitor_states handle_monitor_exit(struct commands_state * ctx,struct selector_key *key) {
+    puts("EXIT");
+    ctx->pop3_current_command->cmd_id = EXIT;
+    ctx->pop3_current_command->is_finished = false;
+    ctx->pop3_current_command->has_error = false;
+    ctx->pop3_current_command->noop_state = true;
+    struct connection_state * data = key->data;
+    if (data->auth_data.user_index != -1){
+        data->args->users[data->auth_data.user_index].close = false;
+    }
   return AUTH_MONITOR;
 }
 enum monitor_states handle_write_exit_monitor(struct selector_key *key, pop3_current_command *current_command, struct commands_state *commands) {
