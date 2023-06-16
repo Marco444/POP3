1a5217dee186f953114b1c0a6a881c5479121d2b
lautaro <lautihernando@gmail.com>
2023-06-16 15:15:13 -0300

    add client

--- client.c
+++ client.c
@@ -5,6 +5,37 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+void flush_buffer(char* buffer, int size) {
+    for (int i = 0; i < size; i++) {
+        buffer[i] = 0;
+    }
+}
+void flus_socket(int socket, char* buffer,int size){
+    int count = 0;
+    int flag = true;
+    int position = 0;
+    while (flag){
+        int rec = recv(socket, (buffer + count), 1024, 0);
+        for (int i = count;i < (count + rec); ++i) {
+            switch (buffer[i]) {
+                case '\r':
+                    position = 1;
+                    break;
+                case '\n':
+                    if (position == 1) {
+                        flag = false;
+                    }
+                    break;
+                default:
+                    position = 0;
+                    break;
+            }
+        }
+        count += rec;
+    }
+}
+
+
 int main(int argc, char** argv) {
     struct client_args args;
     parse_args(argc, argv, &args);
@@ -41,19 +72,24 @@ int main(int argc, char** argv) {
     char  username [255];
     char  password [255];
     sprintf(username, "USERNAME %s\r\n", args.user.name);
-
+    printf(username);
     send(client_fd, username, strlen(username), 0);
-    recv(client_fd, buffer, strlen(username), 0);
+    flus_socket(client_fd, buffer, 1024);
     if(buffer[0] != '+') {
+        printf("LLEGUE Al username\n", buffer);
         printf("Error: %s\n", buffer);
-        return -1;
+        return 1;
     }
     sprintf(password,"PASSWORD %s\r\n", args.user.pass);
     send(client_fd, password, strlen(password), 0);
-    recv(client_fd, buffer, strlen(password), 0);
+    flush_buffer(buffer, 1024);
+    flus_socket(client_fd, buffer, 1024);
+    printf(buffer);
     if(buffer[0] != '+') {
         printf("Error: %s\n", buffer);
-        return -1;
+        return 1;
     }
-    printf("%s\n", buffer);
+
+
+
 }
